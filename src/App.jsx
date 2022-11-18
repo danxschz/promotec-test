45351158eddb5bc23de4818f51661e50a59e9312
Daniel Sanchez <danxschz@gmail.com>
2022-11-18 16:57:24 -0400

    Crear footer y actualizar estilos de header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import Header from "./components/header/Header";
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <main></main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.scss';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>© 2022 My Company. Todos los derechos reservados</p>
+    </footer>
+  )
+}
+
+export default Footer;
